76d029f831c8203d0554873588dc3a010e4189c8
Vlad <yermakov.v.o@gmail.com>
2016-03-06 06:28:44 +0200

    Modife test pages spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,56 +6,42 @@ describe "Static pages" do
 
   let(:base_title) {"My first Rails app"}
 
+  subject {page}
+
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-      visit root_path
-      expect(page).to have_content('Sample App')
-    end
+    before {visit root_path}
 
-    it "should have the title 'Home'" do
-      visit root_path
-      expect(page).to have_title("#{base_title}")
-    end
+    it {should have_content('Sample App')}
+    it {should have_title("#{base_title}")}
+
   end
 
   describe "Help page" do
 
-    it "should have the content 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-    end
+    before {visit help_path}
 
-    it "should have the title 'Help'" do
-      visit help_path
-      expect(page).to have_title("#{base_title} | Help")
-    end
+    it {should have_content('Help')}
+    it {should have_title("#{base_title} | Help")}
+    
   end
 
   describe "About page" do
 
-    it "should have the content 'About Us'" do
-      visit about_path
-      expect(page).to have_content('About Us')
-    end
+    before {visit about_path}
 
-    it "should have the title 'About Us'" do
-      visit about_path
-      expect(page).to have_title("#{base_title} | About Us")
-    end
+    it {should have_content('About Us') }
+    it {should have_title("#{base_title} | About Us")}
+
   end
 
   describe "Contact page" do
 
-    it "should have the content 'About Us'" do
-      visit contact_path
-      expect(page).to have_content('Contact Us')
-    end
+    before {visit contact_path}
 
-    it "should have the title 'Contact Us'" do
-      visit contact_path
-      expect(page).to have_title("#{base_title} | Contact Us")
-    end
+    it {should have_content('Contact Us') }
+    it {should have_title("#{base_title} | Contact Us") }
+    
   end
 
 end
